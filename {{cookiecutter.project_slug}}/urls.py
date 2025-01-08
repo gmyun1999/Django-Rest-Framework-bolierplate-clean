8494b606eb9f59d0b71d61109c6a2061eb9cb517
gmyun1999 <dbsrbals26@gmail.com>
2025-01-08 17:38:43 +0900

    확장자 변경 테스트

--- {{cookiecutter.project_slug}}/urls.py
+++ {{cookiecutter.project_slug}}/urls.py
@@ -1,8 +1,8 @@
-{% raw %}
+
 from django.urls import path, re_path
 from {{ cookiecutter.project_slug }}.views import HealthChecker
 
 urlpatterns = [
     re_path(r"^$", view=HealthChecker.as_view(), name="HealthChecker"),
 ]
-{% endraw %}
+
